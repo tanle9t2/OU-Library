af8cdfdd14de1a1e03e1730d790d0c70d214a683
tanle <fcletan12@gmail.com>
2025-07-10 12:59:34 +0700

    set up project

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from urllib.parse import quote
+
+from dotenv import dotenv_values, load_dotenv
+import cloudinary
+from flask_login import LoginManager
+app = Flask(__name__)
+load_dotenv()
+
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_URL = os.getenv("DB_URL")
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
+
+app.secret_key = "8923yhr9fuwnsejksnpokff@$I_I@$)opfk"
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL % quote(DB_PASSWORD)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+cloudinary.config(
+    cloud_name="duk7gxwvc",
+    api_key="653944787632934",
+    api_secret="GY20iNSIGW6CdrY1s1cDGwMKrqY",
+    secure=True
+)
+
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['PAGE_SIZE'] = 12
+app.config['ORDER'] = 'desc'
+
+app.config["ORDER_PAGE_SIZE"] = 12
+app.config["IMPORT_PAGE_SIZE"] = 20
+app.config["STATISTIC_FRE_PAGE_SIZE"] = 6
+app.config["STATISTIC_REVEN_PAGE_SIZE"] = 5
+app.config["BOOK_PAGE_SIZE"] = 7
+
+db = SQLAlchemy(app=app)
+login = LoginManager(app)
+
